16ea2f7e752f8b49fc85c83b30bd756f55facaa0
Liubey135 <leomuci.yo@gmail.com>
2024-10-14 17:27:30 +0200

    add_account_country

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,47 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Skull from '../components/Skull.vue'
-import AppAccounts from '../components/AppAccounts.vue'
+// import Vue from 'vue'
+// import VueRouter from 'vue-router'
+// import Skull from '../components/Skull.vue'
+// import AppAccounts from '../components/AppAccounts.vue'
 
-Vue.use(VueRouter)
+// Vue.use(VueRouter)
+
+// const routes = [
+//   {
+//     path: '/skull',
+//     name: 'Skull',
+//     component: Skull
+//   },
+//   {
+//     path: '/accounts',
+//     name: 'AppAccounts',
+//     component: AppAccounts
+//   }
+// ]
+
+// const router = new VueRouter({
+//   mode: 'history',
+//   base: process.env.BASE_URL,
+//   routes
+// })
+
+// export default router
+
+
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Skull from '../components/Skull.vue';
+import AppAccounts from '../components/AppAccounts.vue';
+import Home from '../components/Home.vue';  // Import the Home component
+import NewAccount from '../components/NewAccount.vue';  // Import the NewAccount component
+
+Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home  // Home route added
+  },
   {
     path: '/skull',
     name: 'Skull',
@@ -15,6 +51,11 @@ const routes = [
     path: '/accounts',
     name: 'AppAccounts',
     component: AppAccounts
+  },
+  {
+    path: '/new-account',
+    name: 'NewAccount',
+    component: NewAccount  // Route for creating a new account
   }
 ]
 
@@ -24,4 +65,4 @@ const router = new VueRouter({
   routes
 })
 
-export default router
+export default router;
